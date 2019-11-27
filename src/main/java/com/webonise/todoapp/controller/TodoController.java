b591bf5dc9b91517f4df2f0880f5fc68c771da7e
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-11-27 18:58:45 +0530

    added Getmapping on method level

--- src/main/java/com/webonise/todoapp/controller/TodoController.java
+++ src/main/java/com/webonise/todoapp/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,7 +21,7 @@ public class TodoController {
   @Autowired
   private TodoServiceImpl todoService;
 
-  @RequestMapping
+  @GetMapping
   public List<Todo> getTodos() {
     return todoService.getTodos();
   }
